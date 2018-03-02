4aa3f1305cb23fd1e8defe9181af345e758de0a1
Shyam-Chen <shyamchen1994@gmail.com>
2018-03-02 17:41:11 +0800

    3rd Commit

--- test/test.js
+++ test/test.js
@@ -3,14 +3,28 @@ import { mapModelsToState, updateModel } from '../src';
 process.chdir(__dirname);
 
 describe('vuex-bound', () => {
-  it('should handle mapModelsToState', () => {
-    const STATE_NAME = 'foo.bar';
-    const computed = { ...mapModelsToState(STATE_NAME, ['vue', 'vuex']) };
+  it('should handle mapModelsToState for module name', () => {
+    const STATE_NAME = 'foo';
+    const computed = { ...mapModelsToState(STATE_NAME, ['modelName1', 'modelName2']) };
 
     expect(computed).toMatchSnapshot();
   });
 
-  it('should handle updateModel', () => {
+  it('should handle mapModelsToState for sub-module name', () => {
+    const STATE_NAME = 'foo.bar';
+    const computed = { ...mapModelsToState(STATE_NAME, ['modelName1', 'modelName2']) };
+
+    expect(computed).toMatchSnapshot();
+  });
+
+  it('should handle updateModel for module name', () => {
+    const STATE_NAME = 'foo';
+    const mutations = { ...updateModel(STATE_NAME) };
+
+    expect(mutations).toMatchSnapshot();
+  });
+
+  it('should handle updateModel for sub-module name', () => {
     const STATE_NAME = 'foo.bar';
     const mutations = { ...updateModel(STATE_NAME) };
 
